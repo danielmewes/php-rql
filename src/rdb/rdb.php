687061adb1652707323df0905e44cecdd2b40af1
Moshe Brevda <mbrevda@gmail.com>
2015-11-24 11:12:56 +0200

    Add psr4 complaint autloader for users of the global methods

--- src/rdb/rdb.php
+++ src/rdb/rdb.php
@@ -2,6 +2,35 @@
 
 namespace r;
 
-set_include_path(get_include_path() . PATH_SEPARATOR . __DIR__ . '/../src');
+#https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-4-autoloader-examples.md
+spl_autoload_register(function ($class) {
+
+    // project-specific namespace prefix
+    $prefix = 'r\\';
+
+    // base directory for the namespace prefix
+    $base_dir = __DIR__ . '/../../src/';
+
+    // does the class use the namespace prefix?
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        // no, move to the next registered autoloader
+        return;
+    }
+
+    // get the relative class name
+    $relative_class = substr($class, $len);
+
+    // replace the namespace prefix with the base directory, replace namespace
+    // separators with directory separators in the relative class name, append
+    // with .php
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+
+    // if the file exists, require it
+    if (file_exists($file)) {
+        require $file;
+    }
+});
+
 require_once(__DIR__ . '/global.php');
 require_once(__DIR__ . '../../version.php');
