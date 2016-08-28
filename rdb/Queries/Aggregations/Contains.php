fc04dd76fe8d03b8a31ad6e7e021edbedb567b11
ABRAHAM Morgan <morgan@tramicms.net>
2016-08-28 13:47:39 +0200

    PSR-2 compliing

--- rdb/Queries/Aggregations/Contains.php
+++ rdb/Queries/Aggregations/Contains.php
@@ -11,8 +11,7 @@ class Contains extends ValuedQuery
     {
         $this->setPositionalArg(0, $sequence);
 
-        foreach ($values as $k => $value)
-        {
+        foreach ($values as $k => $value) {
             $this->setPositionalArg($k+1, $this->nativeToDatumOrFunction($value));
         }
     }
