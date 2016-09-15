528184da6041cddb8037566ad19edbbe3692f5ad
Lexx <hi@lexx.gr>
2016-09-15 21:39:50 +0300

    [] to array()

--- rdb/ValuedQuery/ValuedQuery.php
+++ rdb/ValuedQuery/ValuedQuery.php
@@ -214,7 +214,7 @@ abstract class ValuedQuery extends Query
     {
         return new Distinct($this, $opts);
     }
-    public function group($fieldOrFunction, $groupOn = [])
+    public function group($fieldOrFunction, $groupOn = array())
     {
         return new Group($this, $fieldOrFunction, $groupOn);
     }
